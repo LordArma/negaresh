178391e64f1c12f516203d0daaea10b1f32b836b
Arma <arma@jangal.co>
2023-03-21 08:02:52 +0330

    add more data about the plugin

--- negaresh.php
+++ negaresh.php
@@ -12,7 +12,11 @@ Text Domain: negaresh
 Domain Path: negaresh-languages
 Author: Lord Arma
 Author URI: http://LordArma.com/
-License: GPL3
+Requires at least: 5.0
+Tested up to: 6.1.1
+Requires PHP: 7.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 use Alirezasedghi\Virastar\Virastar;
