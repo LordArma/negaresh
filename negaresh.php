e1ef9a107533f2af4871b38394365fea67981f27
Arma <arma@jangal.co>
2023-04-02 05:35:51 +0330

    remaster to class base

--- negaresh.php
+++ negaresh.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package Negaresh
- * @version 2.1.2
+ * @version 2.2.2
  */
 /*
 Plugin Name: Negaresh
 Plugin URI: http://wordpress.org/plugins/negaresh/
 Description: Negaresh tries to fix your Farsi (Persian) typos in Wordpress.
-Version: 2.1.2
+Version: 2.2.2
 Text Domain: negaresh
 Domain Path: negaresh-languages
 Author: Lord Arma
@@ -23,63 +23,73 @@ use Alirezasedghi\Virastar\Virastar;
 
 include('Virastar.php');
 
-function fix_farsi_typoes( $content ) {
+class Negaresh {
 
-    $virastar = new Virastar([
-        'normalize_eol' => true,
-        'decode_html_entities' => true,
-        'fix_dashes' => true,
-        'fix_three_dots' => true,
-        'normalize_ellipsis' => true,
-        'normalize_dates' => true,
-        'fix_english_quotes_pairs' => true,
-        'fix_english_quotes' => false,
-        'fix_hamzeh' => true,
-        'fix_hamzeh_arabic' => false,
-        'cleanup_rlm' => true,
-        'cleanup_zwnj' => true,
-        'fix_arabic_numbers' => true,
-        'fix_english_numbers' => true,
-        'fix_numeral_symbols' => true,
-        'fix_misc_non_persian_chars' => true,
-        'fix_punctuations' => true,
-        'fix_question_mark' => true,
-        'fix_prefix_spacing' => true,
-        'fix_suffix_spacing' => true,
-        'fix_suffix_misc' => true,
-        'fix_spacing_for_braces_and_quotes' => false,
-        'fix_spacing_for_punctuations' => true,
-        'fix_diacritics' => true,
-        'remove_diacritics' => false,
-        'fix_persian_glyphs' => true,
-        'fix_misc_spacing' => true,
-        'cleanup_spacing' => true,
-        'cleanup_line_breaks' => true,
-        'cleanup_begin_and_end' => true
-    ]);
-
-    try {
-        $content = $virastar->cleanup($content);
-    } catch (Exception $e) {
-        echo $e->getMessage();
+    function __construct(){
+        add_action( 'admin_menu', array($this, 'negaresh_menu') );
+        add_filter( 'the_content', array($this, 'fix_farsi_typoes') );
     }
 
-    return nl2br($content ?? '');
+    function fix_farsi_typoes( $content ) {
+
+        $virastar = new Virastar([
+            'normalize_eol' => true,
+            'decode_html_entities' => true,
+            'fix_dashes' => true,
+            'fix_three_dots' => true,
+            'normalize_ellipsis' => true,
+            'normalize_dates' => true,
+            'fix_english_quotes_pairs' => true,
+            'fix_english_quotes' => false,
+            'fix_hamzeh' => true,
+            'fix_hamzeh_arabic' => false,
+            'cleanup_rlm' => true,
+            'cleanup_zwnj' => true,
+            'fix_arabic_numbers' => true,
+            'fix_english_numbers' => true,
+            'fix_numeral_symbols' => true,
+            'fix_misc_non_persian_chars' => true,
+            'fix_punctuations' => true,
+            'fix_question_mark' => true,
+            'fix_prefix_spacing' => true,
+            'fix_suffix_spacing' => true,
+            'fix_suffix_misc' => true,
+            'fix_spacing_for_braces_and_quotes' => false,
+            'fix_spacing_for_punctuations' => true,
+            'fix_diacritics' => true,
+            'remove_diacritics' => false,
+            'fix_persian_glyphs' => true,
+            'fix_misc_spacing' => true,
+            'cleanup_spacing' => true,
+            'cleanup_line_breaks' => true,
+            'cleanup_begin_and_end' => true
+        ]);
+
+        try {
+            $content = $virastar->cleanup($content);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        return nl2br($content ?? '');
+    }
+
+    function negaresh_menu() {
+        add_options_page( 'Negaresh Options', 'Negaresh', 'manage_options', 'negaresh', array($this, 'negaresh_html') );
+    }
+
+    function negaresh_html() {
+        if ( !current_user_can( 'manage_options' ) )  {
+            wp_die( __( 'You do not have sufficient permissions to access this page.', 'negaresh' ) );
+        }
+        echo '<div class="wrap"><p>';
+        echo '<h1>';
+        _e('Negaresh Options');
+        echo '</h1>';
+        _e("Currently, Negaresh doesn't support options. This feature will be added to Negaresh soon.", "negaresh");
+        echo '</p></div>';
+    }
+
 }
 
-add_filter( 'the_content', 'fix_farsi_typoes' );
-
-function negaresh_menu() {
-	add_options_page( 'Negaresh Options', 'Negaresh', 'manage_options', 'negaresh', 'negaresh_options' );
-}
-
-function negaresh_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.', 'negaresh' ) );
-	}
-	echo '<div class="wrap"><p>';
-	_e("Currently, Negaresh doesn't support options. This feature will be added to Negaresh soon.", "negaresh");
-	echo '</p></div>';
-}
-
-add_action( 'admin_menu', 'negaresh_menu' );
+$negaresh = new Negaresh();
